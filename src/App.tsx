45aed0e59f88f7312f38c87790880d72d39ed2c7
Will Harris <will.t.harris@protonmail.com>
2019-12-01 13:19:41 -0700

    feat: initial wiring of mapbox map, map renders

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import "./App.scss";
 
 const App: React.FC = () => {
+  const [mapState, setMapState] = useState({
+    lng: 5,
+    lat: 34,
+    zoom: 2
+  });
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [mapState.lng, mapState.lat],
+      zoom: mapState.zoom
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mapContainer" id="map" />
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import mapboxgl from "mapbox-gl";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoid2hhcnJpcyIsImEiOiJjazNhdmdhNDAwMzVwM21xaXFrbTRzc2x3In0.n5nQB9llpSat6TrVQMFn3g";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
