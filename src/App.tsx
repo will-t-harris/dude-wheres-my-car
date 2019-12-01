aeb2cf9802d769113ab6a7b3ab73bcc730abeefd
Will Harris <will.t.harris@protonmail.com>
2019-12-01 16:02:48 -0700

    refactor: remove data overlay implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,12 @@
 import React, { useState, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
-// import data from "./data.json";
 import "./App.scss";
-const data: mapboxgl.MapboxGeoJSONFeature = require("./data.json");
-
-const options = [
-  {
-    name: "Population",
-    description: "Estimated total population",
-    property: "pop_est",
-    stops: [
-      [0, "#f8d5cc"],
-      [1000000, "#f4bfb6"],
-      [5000000, "#f1a8a5"],
-      [10000000, "#ee8f9a"],
-      [50000000, "#ec739b"],
-      [100000000, "#dd5ca8"],
-      [250000000, "#c44cc0"],
-      [500000000, "#9f43d7"],
-      [1000000000, "#6e40e6"]
-    ]
-  },
-  {
-    name: "GDP",
-    description: "Estimate total GDP in millions of dollars",
-    property: "gdp_md_est",
-    stops: [
-      [0, "#f8d5cc"],
-      [1000, "#f4bfb6"],
-      [5000, "#f1a8a5"],
-      [10000, "#ee8f9a"],
-      [50000, "#ec739b"],
-      [100000, "#dd5ca8"],
-      [250000, "#c44cc0"],
-      [5000000, "#9f43d7"],
-      [10000000, "#6e40e6"]
-    ]
-  }
-];
 
 const App: React.FC = () => {
   const [mapState, setMapState] = useState<any>({
     lng: 5,
     lat: 34,
-    zoom: 2,
-    active: options[0]
+    zoom: 2
   });
 
   useEffect(() => {
@@ -53,31 +15,6 @@ const App: React.FC = () => {
       style: "mapbox://styles/mapbox/streets-v11",
       center: [mapState.lng, mapState.lat],
       zoom: mapState.zoom
-    });
-
-    const setFill = () => {
-      const { property, stops } = mapState.active;
-      map.setPaintProperty("countries", "fill-color", {
-        property,
-        stops
-      });
-    };
-
-    map.on("load", () => {
-      map.addSource("countries", {
-        type: "geojson",
-        data: data
-      });
-
-      map.addLayer(
-        {
-          id: "countries",
-          type: "fill",
-          source: "countries"
-        },
-        "country-label-lg"
-      );
-      setFill();
     });
 
     map.on("move", () => {
